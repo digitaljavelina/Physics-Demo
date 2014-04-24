e2145e8d6489961a1269f20c943eb7a502a1b564
Michael Henry <mididoc@cox.net>
2014-04-24 13:35:39 -0700

    Initial Commit

---
+++ PhysicsDEmo/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  PhysicsDEmo
+//
+
+//  Copyright (c) 2014 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
